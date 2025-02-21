a677520b4c1a14974bb7e1b08467be84e3635114
Erik Krysen <er8871kr-s@student.lu.se>
2025-02-21 11:03:12 +0100

    Added blur effect when hovering photography button

--- js/filter.js
+++ js/filter.js
@@ -1,3 +1,18 @@
-function blur() {
+document.addEventListener("DOMContentLoaded", () => {
+    const elementsToBlur = document.querySelectorAll('.textArea, .list, .undercontent, .buttons.code, .buttons.design');
+    function blurElements() {
+        elementsToBlur.forEach(element => {
+            element.classList.add('blurred');
+        });
+      }
+  
+    function removeBlur() {
+      elementsToBlur.forEach(element => {
+        element.classList.remove('blurred');
+      });
+    }
     
-}
+    const focusphoto = document.getElementById('photobutton');
+    focusphoto.addEventListener('mouseenter', () => blurElements());
+    focusphoto.addEventListener('mouseout', removeBlur);
+});
